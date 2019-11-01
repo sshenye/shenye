c5cd7891e66a954cc98ad84203a399068ec94de0
shenye <saishenye@icloud.com>
2019-11-01 11:21:45 +0800

    这是一个测试

--- chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter1/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -10,5 +10,6 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("you are  sb");
 	}
 }
